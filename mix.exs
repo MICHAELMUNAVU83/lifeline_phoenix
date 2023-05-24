a0036331bb9dcaa1ff0f21cdab2c5a7ce43bbee2
MICHAELMUNAVU83 <michaelmunavu83.com>
2023-05-24 15:08:49 +0300

    Setup

--- mix.exs
+++ mix.exs
@@ -50,7 +50,8 @@ defmodule LifelinePhoenix.MixProject do
       {:telemetry_poller, "~> 1.0"},
       {:gettext, "~> 0.18"},
       {:jason, "~> 1.2"},
-      {:plug_cowboy, "~> 2.5"}
+      {:plug_cowboy, "~> 2.5"},
+      {:ex_fontawesome, "~> 0.7.2"}
     ]
   end
 
